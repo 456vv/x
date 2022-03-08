51705efee3dcc597b411f8a27ab9e5e2115afed2
456vv <123@456vv.com>
2022-03-08 11:22:27 +0800

    no message

--- vweb_dynamic/default.go
+++ vweb_dynamic/default.go
@@ -9,6 +9,7 @@ func DefaultPlus() map[string]vweb.DynamicTemplateFunc {
 		"ank": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Anko{}}),
 		"yaegi": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Yaegi{}}),
 		"template": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Template{}}),
+		"gossa": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Gossa{}}),
 	}
 	return plus
 }
