508ad88ca5bc9e1299e7dee42330ddad16e407eb
456vv <123@456vv.com>
2022-04-09 22:03:59 +0800

    no message

--- vweb_dynamic/default.go
+++ vweb_dynamic/default.go
@@ -2,14 +2,24 @@ package vweb_dynamic
 
 import (
 	"github.com/456vv/vweb/v2"
+	"github.com/456vv/viot/v2"
 )
 
-func DefaultPlus() map[string]vweb.DynamicTemplateFunc {
-	plus := map[string]vweb.DynamicTemplateFunc{
+func WEBModule() map[string]vweb.DynamicTemplateFunc {
+	module := map[string]vweb.DynamicTemplateFunc{
 		"ank": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Anko{}}),
 		"yaegi": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Yaegi{}}),
 		"template": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Template{}}),
 		"gossa": vweb.DynamicTemplateFunc(func(D *vweb.ServerHandlerDynamic) vweb.DynamicTemplater {return &Gossa{}}),
 	}
-	return plus
+	return module
+}
+func IOTModule() map[string]viot.DynamicTemplateFunc {
+	module := map[string]viot.DynamicTemplateFunc{
+		"ank": viot.DynamicTemplateFunc(func(D *viot.ServerHandlerDynamic) viot.DynamicTemplater {return &Anko{}}),
+		"yaegi": viot.DynamicTemplateFunc(func(D *viot.ServerHandlerDynamic) viot.DynamicTemplater {return &Yaegi{}}),
+		"template": viot.DynamicTemplateFunc(func(D *viot.ServerHandlerDynamic) viot.DynamicTemplater {return &Template{}}),
+		"gossa": viot.DynamicTemplateFunc(func(D *viot.ServerHandlerDynamic) viot.DynamicTemplater {return &Gossa{}}),
+	}
+	return module
 }
