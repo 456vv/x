020cd3c0a9f3af67fcf457610883685e631af277
456vv <123@456vv.com>
2023-02-20 12:29:03 +0800

    增加 DataTo

--- db/db.go
+++ db/db.go
@@ -1,15 +1,18 @@
-package	db
-import(
-	"database/sql"
-	_ "github.com/lib/pq"
+package db
+
+import (
 	"context"
-	"strings"
-	"reflect"
-	"time"
+	"database/sql"
 	"errors"
-	"log"
 	"fmt"
+	"log"
+	"reflect"
+	"strings"
+	"time"
+
+	_ "github.com/lib/pq"
 )
+
 /*
 	DB.Open(driverName,	dataSourceName string) (*DB, error)
 	DB.Close() error
@@ -27,25 +30,26 @@ import(
 
 var errDebugResult = errors.New("the debug result type does not match the returned result type")
 
-type Rower interface{
+type Rower interface {
 	Scan(dest ...interface{}) error
 	Err() error
 }
-type dbRow struct{
-	tx	*sql.Tx
-	r	*sql.Row
-	err	error
+type dbRow struct {
+	tx     *sql.Tx
+	r      *sql.Row
+	err    error
 	cancel context.CancelFunc
 }
-func (T	*dbRow)	Scan(dest ...interface{}) error	{
+
+func (T *dbRow) Scan(dest ...interface{}) error {
 	if T.cancel != nil {
 		defer T.cancel()
 	}
-	if T.err !=	nil	{
+	if T.err != nil {
 		return T.err
 	}
-	err	:= T.r.Scan(dest...)
-	if T.tx	== nil {
+	err := T.r.Scan(dest...)
+	if T.tx == nil {
 		return err
 	}
 	if err != nil {
@@ -54,37 +58,39 @@ func (T	*dbRow)	Scan(dest ...interface{}) error	{
 	}
 	return T.tx.Commit()
 }
-func (T	*dbRow)	Err() error{
+
+func (T *dbRow) Err() error {
 	return T.err
 }
 
-//说明：https://github.com/lib/pq/blob/master/doc.go
-//postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
-type DB	struct{
+// 说明：https://github.com/lib/pq/blob/master/doc.go
+// postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
+type DB struct {
 	*sql.DB
-	Timeout	time.Duration
-	FormatColumnName bool			//将例名 AbcDf	=> abcDf
-	TypeTo		func(ct *sql.ColumnType) reflect.Type
-	ErrorLog	*log.Logger
-	DebugPrint	bool
-	DebugResult func(tx	*sql.Tx, ctx context.Context, sqlstr string, args ... interface{}) interface{}
-	driverName	string
+	Timeout          time.Duration
+	FormatColumnName bool // 将例名 AbcDf	=> abcDf
+	TypeTo           func(*sql.ColumnType) reflect.Type
+	DataTo           func(string, interface{}) interface{}
+	ErrorLog         *log.Logger
+	DebugPrint       bool
+	DebugResult      func(tx *sql.Tx, ctx context.Context, sqlstr string, args ...interface{}) interface{}
+	driverName       string
 }
 
-
-func (T	*DB) Open(driverName, dataSourceName string) (*DB, error) {
-	var	err	error
-	if T.DB	== nil {
-		T.driverName=driverName
-		T.DB, err =	sql.Open(driverName, dataSourceName)
+func (T *DB) Open(driverName, dataSourceName string) (*DB, error) {
+	var err error
+	if T.DB == nil {
+		T.driverName = driverName
+		T.DB, err = sql.Open(driverName, dataSourceName)
 	}
 	return T, err
 }
-func (T	*DB) Close() error {
-	if T.DB	== nil {
+
+func (T *DB) Close() error {
+	if T.DB == nil {
 		return sql.ErrConnDone
 	}
-	err	:= T.DB.Close()
+	err := T.DB.Close()
 	T.DB = nil
 	return err
 }
@@ -94,12 +100,12 @@ func (T *DB) txCommit(tx *sql.Tx, e *error) {
 		if err := tx.Rollback(); err != nil {
 			T.logf(err.Error())
 		}
-	}else if err := tx.Commit(); err != nil {
+	} else if err := tx.Commit(); err != nil {
 		T.logf(err.Error())
 	}
 }
 
-//日志
+// 日志
 func (T *DB) logf(format string, a ...interface{}) {
 	if T.ErrorLog != nil {
 		T.ErrorLog.Output(2, fmt.Sprintf(format+"\n", a...))
@@ -108,244 +114,272 @@ func (T *DB) logf(format string, a ...interface{}) {
 }
 
 func (T *DB) debugFormat(sqlStr string) string {
-	sqlStr =  strings.ReplaceAll(sqlStr, "\n", " ")
-	sqlStr =  strings.ReplaceAll(sqlStr, "\t", "")
-	sqlStr =  strings.ReplaceAll(sqlStr, "  ", " ")
+	sqlStr = strings.ReplaceAll(sqlStr, "\n", " ")
+	sqlStr = strings.ReplaceAll(sqlStr, "\t", "")
+	sqlStr = strings.ReplaceAll(sqlStr, "  ", " ")
 	return sqlStr
 }
-//打印调试信息
-func (T *DB) debugPrint(sqlStr string, args interface{}){
+
+// 打印调试信息
+func (T *DB) debugPrint(sqlStr string, args interface{}) {
 	if T.DebugPrint {
 		sqlStr = T.debugFormat(sqlStr)
 		rv := reflect.ValueOf(args)
 		rv = reflect.Indirect(rv)
 		var argsStr []string
 		if rv.Kind() == reflect.Slice {
-			for i:=0;i<rv.Len();i++ {
+			for i := 0; i < rv.Len(); i++ {
 				srv := rv.Index(i)
-				argsStr = append(argsStr, fmt.Sprintf("'%v'",inDirect(srv)))
+				argsStr = append(argsStr, fmt.Sprintf("'%v'", inDirect(srv)))
 			}
 		}
-		T.logf("\nprepare test as %s;\nexecute test(%s);\ndeallocate test;\n", sqlStr, strings.Join(argsStr,","))
+		T.logf("\nprepare test as %s;\nexecute test(%s);\ndeallocate test;\n", sqlStr, strings.Join(argsStr, ","))
 	}
 }
 
-//支持写入(postgresql)，支持返回错误：error, nil, ErrNoRows
-func (T	*DB) Pexec(tx *sql.Tx,	sqlstr string, args	...interface{})	error {
-	ctx	:= context.Background()
-	var	cancel context.CancelFunc
-	if T.Timeout !=	0 {
-		ctx, cancel	= context.WithTimeout(ctx, T.Timeout)
+// 支持写入(postgresql)，支持返回错误：error, nil, ErrNoRows
+func (T *DB) Pexec(tx *sql.Tx, sqlstr string, args ...interface{}) error {
+	ctx := context.Background()
+	var cancel context.CancelFunc
+	if T.Timeout != 0 {
+		ctx, cancel = context.WithTimeout(ctx, T.Timeout)
 		defer cancel()
 	}
-	return T.PexecContext(tx, ctx,	sqlstr,	args...)
+	return T.PexecContext(ctx, tx, sqlstr, args...)
 }
-func (T	*DB) PexecContext(tx *sql.Tx, ctx context.Context,	sqlstr string, args	...interface{})	(err error)	{
-	result,	err	:= T.ExecContext(tx, ctx, sqlstr, args...)
+
+func (T *DB) PexecContext(ctx context.Context, tx *sql.Tx, sqlstr string, args ...interface{}) (err error) {
+	result, err := T.ExecContext(ctx, tx, sqlstr, args...)
 	if err != nil {
 		return
 	}
-	
+
 	affected, err := result.RowsAffected()
 	if err != nil {
-		return 
+		return
 	}
-	if affected	== 0 {
+	if affected == 0 {
 		return sql.ErrNoRows
 	}
 	return
 }
 
-//支持写入
-func (T	*DB) Exec(tx *sql.Tx, sqlstr string, args ...interface{}) (result sql.Result, err error) {
-	ctx	:= context.Background()
-	var	cancel context.CancelFunc
-	if T.Timeout !=	0 {
-		ctx, cancel	= context.WithTimeout(ctx, T.Timeout)
+// 支持写入
+func (T *DB) Exec(tx *sql.Tx, sqlstr string, args ...interface{}) (result sql.Result, err error) {
+	ctx := context.Background()
+	var cancel context.CancelFunc
+	if T.Timeout != 0 {
+		ctx, cancel = context.WithTimeout(ctx, T.Timeout)
 		defer cancel()
 	}
-	return T.ExecContext(tx, ctx, sqlstr, args...)
+	return T.ExecContext(ctx, tx, sqlstr, args...)
 }
-func (T	*DB) ExecContext(tx	*sql.Tx, ctx context.Context, sqlstr string, args ...interface{}) (result sql.Result, err error) {
-	if T.DB	== nil {
-		return nil,	sql.ErrConnDone
+
+func (T *DB) ExecContext(ctx context.Context, tx *sql.Tx, sqlstr string, args ...interface{}) (result sql.Result, err error) {
+	if T.DB == nil {
+		return nil, sql.ErrConnDone
 	}
-	
+
 	T.debugPrint(sqlstr, args)
 	if T.DebugResult != nil {
 		if v, ok := T.DebugResult(tx, ctx, T.debugFormat(sqlstr), args...).(sql.Result); ok {
 			return v, nil
 		}
 	}
-	
+
 	if tx == nil {
-		tx,	err	= T.DB.Begin()
+		tx, err = T.DB.Begin()
 		if err != nil {
-			return nil,	err
+			return nil, err
 		}
 		defer T.txCommit(tx, &err)
 	}
-	result,	err	= tx.ExecContext(ctx, sqlstr, args...)
+	result, err = tx.ExecContext(ctx, sqlstr, args...)
 	return
 }
 
-//支持读取/写入，支持返回错误：error, nil, ErrNoRows
-func (T	*DB) QueryRow(tx *sql.Tx, sqlstr string, args ...interface{}) Rower {
-	ctx	:= context.Background()
-	return T.QueryRowContext(tx, ctx, sqlstr, args...)
+// 支持读取/写入，支持返回错误：error, nil, ErrNoRows
+func (T *DB) QueryRow(tx *sql.Tx, sqlstr string, args ...interface{}) Rower {
+	return T.QueryRowContext(context.Background(), tx, sqlstr, args...)
 }
-func (T	*DB) QueryRowContext(tx	*sql.Tx, ctx context.Context, sqlstr string, args ...interface{}) Rower {
-	if T.DB	== nil {
-		return &dbRow{err:sql.ErrConnDone}
+
+func (T *DB) QueryRowContext(ctx context.Context, tx *sql.Tx, sqlstr string, args ...interface{}) Rower {
+	if T.DB == nil {
+		return &dbRow{err: sql.ErrConnDone}
 	}
-	
+
 	T.debugPrint(sqlstr, args)
 	if T.DebugResult != nil {
 		if v, ok := T.DebugResult(tx, ctx, T.debugFormat(sqlstr), args...).(Rower); ok {
 			return v
 		}
 	}
-	
-	//查询，用不上回滚
-	var sqlstrTL = strings.ToLower(strings.TrimLeft(sqlstr, " \t\r\n"))
-	if strings.HasPrefix(sqlstrTL, "select")	{
+
+	// 查询，用不上回滚
+	sqlstrTL := strings.ToLower(strings.TrimLeft(sqlstr, " \t\r\n"))
+	if strings.HasPrefix(sqlstrTL, "select") {
 		return T.DB.QueryRowContext(ctx, sqlstr, args...)
 	}
-	
-	//执行，需要回滚
-	var	sqlTx *sql.Tx
+
+	// 执行，需要回滚
+	var sqlTx *sql.Tx
 	if tx == nil {
-		var	err	error
-		tx,	err	= T.DB.Begin()
+		var err error
+		tx, err = T.DB.Begin()
 		if err != nil {
-			return &dbRow{err:err}
+			return &dbRow{err: err}
 		}
-		sqlTx =	tx
+		sqlTx = tx
 	}
-	
-	var cancel context.CancelFunc = func(){}
-	if T.Timeout !=	0 {
-		//这个上下文没有设置超时
+
+	var cancel context.CancelFunc = func() {}
+	if T.Timeout != 0 {
+		// 这个上下文没有设置超时
 		if _, ok := ctx.Deadline(); !ok {
-			ctx, cancel	= context.WithTimeout(ctx, T.Timeout)
+			ctx, cancel = context.WithTimeout(ctx, T.Timeout)
 		}
 	}
-	return &dbRow{tx:sqlTx,	r:tx.QueryRowContext(ctx, sqlstr, args...), cancel:cancel}
+	return &dbRow{tx: sqlTx, r: tx.QueryRowContext(ctx, sqlstr, args...), cancel: cancel}
 }
 
-//支持读取/写入，支持返回错误：error, nil, ErrNoRows
-func (T	*DB) Query(tx *sql.Tx, sqlstr string, args ...interface{}) (data []map[string]interface{}, err error) {
-	ctx	:= context.Background()
-	var	cancel context.CancelFunc
-	if T.Timeout !=	0 {
-		ctx, cancel	= context.WithTimeout(ctx, T.Timeout)
+// 支持读取/写入，支持返回错误：error, nil, ErrNoRows
+func (T *DB) Query(tx *sql.Tx, sqlstr string, args ...interface{}) (data []map[string]interface{}, err error) {
+	ctx := context.Background()
+	var cancel context.CancelFunc
+	if T.Timeout != 0 {
+		ctx, cancel = context.WithTimeout(ctx, T.Timeout)
 		defer cancel()
 	}
-	return T.QueryContext(tx, ctx, sqlstr, args...)
+	return T.QueryContext(ctx, tx, sqlstr, args...)
 }
-func (T	*DB) QueryContext(tx *sql.Tx, ctx context.Context, sqlstr string, args ...interface{}) (data []map[string]interface{}, err error) {
-	if T.DB	== nil {
-		return nil,	sql.ErrConnDone
+
+func (T *DB) QueryContext(ctx context.Context, tx *sql.Tx, sqlstr string, args ...interface{}) (data []map[string]interface{}, err error) {
+	if T.DB == nil {
+		return nil, sql.ErrConnDone
 	}
-	
+
 	T.debugPrint(sqlstr, args)
 	if T.DebugResult != nil {
 		if v, ok := T.DebugResult(tx, ctx, T.debugFormat(sqlstr), args...).([]map[string]interface{}); ok {
 			return v, nil
 		}
 	}
-	
+
 	var (
 		sqlstrTL = strings.ToLower(strings.TrimLeft(sqlstr, " \t\r\n"))
-		rows *sql.Rows
-	)	
+		rows     *sql.Rows
+	)
 	if strings.HasPrefix(sqlstrTL, "select") {
-		//查询，有返回
-		rows, err =	T.DB.QueryContext(ctx, sqlstr, args...)
+		// 查询，有返回
+		rows, err = T.DB.QueryContext(ctx, sqlstr, args...)
 		if err != nil {
-			return nil,	err
+			return nil, err
 		}
-	}else if strings.Index(sqlstrTL, "returning") == -1	{
-		//插入或更新，没有返回
-		return nil,	T.PexecContext(tx,	ctx, sqlstr, args...)
-	}else{
-		//插入或更新，有返回
+	} else if !strings.Contains(sqlstrTL, "returning") {
+		// 插入或更新，没有返回
+		return nil, T.PexecContext(ctx, tx, sqlstr, args...)
+	} else {
+		// 插入或更新，有返回
 		if tx == nil {
-			tx,	err	= T.DB.Begin()
+			tx, err = T.DB.Begin()
 			if err != nil {
-				return nil,	err
+				return nil, err
 			}
 			defer T.txCommit(tx, &err)
 		}
-		
-		rows, err =	tx.QueryContext(ctx, sqlstr, args...)
+
+		rows, err = tx.QueryContext(ctx, sqlstr, args...)
 		if err != nil {
-			return nil,	err
+			return nil, err
 		}
 		defer rows.Close()
 	}
 
-	//列名处理
-	columns, err :=	rows.Columns()
+	// 类型转换
+	typeToFunc, ok := ctx.Value(TypeToContextKey).(func(*sql.ColumnType) reflect.Type)
+	typeTo := func(ct *sql.ColumnType) reflect.Type {
+		var t reflect.Type
+		if ok {
+			t = typeToFunc(ct)
+		}
+		if t == nil && T.TypeTo != nil {
+			return T.TypeTo(ct)
+		}
+		return t
+	}
+
+	// 数据转换
+	dataToFunc, ok := ctx.Value(DataToContextKey).(func(string, interface{}) interface{})
+	dataTo := func(s string, inf interface{}) interface{} {
+		if ok {
+			inf = dataToFunc(s, inf)
+		}
+		if T.DataTo != nil {
+			return T.DataTo(s, inf)
+		}
+		return inf
+	}
+
+	// 列名处理
+	columns, err := rows.Columns()
 	if err != nil {
-		return nil,	err
+		return nil, err
 	}
 	if T.FormatColumnName {
-		for	index, col := range	columns	{
-			columns[index]=keyToLower(col)
+		for index, col := range columns {
+			columns[index] = keyToLower(col)
 		}
 	}
-	
-	//值处理
-	columnTypes, err :=	rows.ColumnTypes();
+
+	// 值处理
+	columnTypes, err := rows.ColumnTypes()
 	if err != nil {
-		return nil,	err
+		return nil, err
 	}
-	var	values = make([]interface{}, len(columnTypes))
-	for	i, tp := range columnTypes {
-		st := tp.ScanType()
-		if st == nil {
-			st = reflect.TypeOf((*interface{})(nil))
+	values := make([]interface{}, len(columnTypes))
+	for i, columnType := range columnTypes {
+		scanType := columnType.ScanType()
+		if tto := typeTo(columnType); tto != nil {
+			scanType = tto
 		}
-		if T.TypeTo != nil {
-			if tto := T.TypeTo(tp); tto != nil {
-				st = tto
-			}
-			
+		if scanType == nil {
+			scanType = reflect.TypeOf((*interface{})(nil))
 		}
-		values[i] =	reflect.New(st).Interface()
+		values[i] = reflect.New(scanType).Interface()
 	}
-	
-	//读出例值
-	for	rows.Next()	{
-		err	= rows.Scan(values...)
+
+	// 读出例值
+	for rows.Next() {
+		err = rows.Scan(values...)
 		if err != nil {
-			return nil,	err
+			return nil, err
 		}
 		column := make(map[string]interface{})
-		data = append(data,	column)
-		for	i, value :=	range values {
-			column[columns[i]] = reflect.Indirect( reflect.ValueOf(value) ).Interface()
+		data = append(data, column)
+		for i, value := range values {
+			value = reflect.ValueOf(value).Elem().Interface()
+			column[columns[i]] = dataTo(columns[i], value)
 		}
 	}
-	if data	== nil {
-		err	= sql.ErrNoRows
+	if data == nil {
+		err = sql.ErrNoRows
 	}
 	return
 }
 
-//判断数据存在，支持返回错误：error, nil, ErrNoRows
-func (T	*DB) Has(sqlstr	string,	args ...interface{}) error {
-	ctx	:= context.Background()
-	var	cancel context.CancelFunc
-	if T.Timeout !=	0 {
-		ctx, cancel	= context.WithTimeout(ctx, T.Timeout)
+// 判断数据存在，支持返回错误：error, nil, ErrNoRows
+func (T *DB) Has(sqlstr string, args ...interface{}) error {
+	ctx := context.Background()
+	var cancel context.CancelFunc
+	if T.Timeout != 0 {
+		ctx, cancel = context.WithTimeout(ctx, T.Timeout)
 		defer cancel()
 	}
 	return T.HasContext(ctx, sqlstr, args...)
 }
-func (T	*DB) HasContext(ctx	context.Context, sqlstr	string,	args ...interface{}) error {
-	if T.DB	== nil {
+
+func (T *DB) HasContext(ctx context.Context, sqlstr string, args ...interface{}) error {
+	if T.DB == nil {
 		return sql.ErrConnDone
 	}
 	T.debugPrint(sqlstr, args)
@@ -354,10 +388,10 @@ func (T	*DB) HasContext(ctx	context.Context, sqlstr	string,	args ...interface{})
 			return v
 		}
 	}
-	
-	var	err	error
-	var	rows *sql.Rows
-	rows, err =	T.DB.QueryContext(ctx, sqlstr, args...)
+
+	var err error
+	var rows *sql.Rows
+	rows, err = T.DB.QueryContext(ctx, sqlstr, args...)
 	if err != nil {
 		return err
 	}
@@ -367,3 +401,15 @@ func (T	*DB) HasContext(ctx	context.Context, sqlstr	string,	args ...interface{})
 	}
 	return nil
 }
+
+// 上下文的Key, 在请求中可以使用
+type contextKey struct {
+	name string
+}
+
+func (T *contextKey) String() string { return "db context value " + T.name }
+
+var (
+	TypeToContextKey = &contextKey{"TypeTo"}
+	DataToContextKey = &contextKey{"DataTo"}
+)
--- db/func.go
+++ db/func.go
@@ -3,6 +3,55 @@ import(
     "strings"
     "reflect"
 )
+
+//过滤指定例
+//	m []map[string]interface{}	数据库的数据
+//	f func(string) bool			返回true,过滤该例
+//	[]map[string]interface{}	过滤后的数据
+func ColumnFilter(m []map[string]interface{}, f func(string) bool) []map[string]interface{}{
+	var retn []map[string]interface{}
+	for _, mv := range m {
+		nm := make(map[string]interface{})
+		for n, v := range mv {
+			if f(n) {
+				continue
+			}
+			nm[n]=v
+		}
+		retn = append(retn, nm)
+	}
+	return retn
+}
+
+//以数组形式返回key列的值
+//	m []map[string]interface{}	数据库的数据
+//	key string					key键名
+//	[]interface{}				返回key对应的值，不包含nil值
+func ColumnArray(m []map[string]interface{}, key string) []interface{}{
+	var vals []interface{}
+	 for _, mv := range m {
+	 	if v, ok := mv[key]; ok {
+	 		if v != nil {
+	 			vals = append(vals, v)
+	 		}
+	 	}
+	 }
+	 return vals
+}
+//以数组形式返回key列的值
+//	m []map[string]interface{}	数据库的数据
+//	key string					key键名
+//	[]interface{}				返回key对应的值，包含nil值
+func ColumnArrayNil(m []map[string]interface{}, key string) []interface{}{
+	var vals []interface{}
+	 for _, mv := range m {
+	 	if v, ok := mv[key]; ok {
+	 		vals = append(vals, v)
+	 	}
+	 }
+	 return vals
+}
+
 //map键转小写
 func keyToLower(key string) string {
 	var nkey string = key
