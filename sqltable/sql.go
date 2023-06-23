e7daa151690d6f52403ce3781fb343d2059d7fd8
456vv <123@456vv.com>
2023-06-23 19:41:51 +0800

    sqltable 修复嵌套错误

--- sqltable/sql.go
+++ sqltable/sql.go
@@ -1,10 +1,11 @@
 package sqltable
 
 import (
-	"strings"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
 //使用方法：
 //
 //Prepare(`select "A1" from "A" $Where$ order by "ID" limit 10`).And("A1=?", value)
@@ -27,90 +28,89 @@ import (
 //Value(column string, value interface{}) *SQLTable
 //ColumnMark(q string) *SQLTable
 
-
-type SQLTable struct{
-	source		string								//未处理的sql语句
-	result		string								//最终sql语句
-	swhere		[]*sqlTableWhere
-	sexcluded 	[]string
-	count		int
-	args		[]interface{}						//参数
-	extArgs		[]interface{}
-	columnMark	string
+type SQLTable struct {
+	source     string // 未处理的sql语句
+	result     string // 最终sql语句
+	swhere     []*sqlTableWhere
+	sexcluded  []string
+	count      int
+	args       []interface{} // 参数
+	extArgs    []interface{}
+	columnMark string
 	valuesKeys []string
 	valuesVals []interface{}
-	setKeys	   []string
-	setVals	   []interface{}
-	types		map[string]string
+	setKeys    []string
+	setVals    []interface{}
+	types      map[string]string
 }
 
 func Prepare(s string) *SQLTable {
 	st := &SQLTable{
-		types:make(map[string]string),
+		types: make(map[string]string),
 	}
 
-	for i:=1;true;i++ {
-		dollar := fmt.Sprint("$",i)
+	for i := 1; true; i++ {
+		dollar := fmt.Sprint("$", i)
 		if !strings.Contains(s, dollar) {
 			break
 		}
-		s = strings.ReplaceAll(s, dollar, fmt.Sprint("@",i))
+		s = strings.ReplaceAll(s, dollar, fmt.Sprint("@", i))
 	}
 	st.source = s
 	return st
 }
 
 func (T *SQLTable) progress(val interface{}) string {
-	if statement, ok := val.(*SQLTable); ok {
-		statement.count += T.count
-		s := "("+statement.SQL()+")"
+	if t, ok := val.(*SQLTable); ok {
+		statement := t.clone()
+		statement.count = T.count
+		s := "(" + statement.SQL() + ")"
 		T.count = statement.count
-		statement.count = 0
 		T.args = append(T.args, statement.args...)
+
 		return s
 	}
 	T.count++
 	T.args = append(T.args, val)
-	return "$"+strconv.Itoa(T.count)
+	return "$" + strconv.Itoa(T.count)
 }
 
-//支持设置转换类型
+// 支持设置转换类型
 func (T *SQLTable) ToTypes(a ...interface{}) *SQLTable {
 	T.pairArgs(func(i int, k, v interface{}) bool {
 		key, ok := k.(string)
 		if !ok {
-			panic(fmt.Sprintf("SQLTable.ToTypes in %d, 键名是%#v，不是字符串!", i, k))
-			return true
+			panic(fmt.Sprintf("SQLTable.ToTypes in %d, 键名是%#v, 不是字符串!", i, k))
 		}
 		val, ok := v.(string)
 		if !ok {
-			panic(fmt.Sprintf("SQLTable.ToTypes in %d, 键值是%#v，不是字符串!", i, v))
-			return true
+			panic(fmt.Sprintf("SQLTable.ToTypes in %d, 键值是%#v, 不是字符串!", i, v))
 		}
 		keyName := T.addColumnMark(key)
-		T.types[keyName]=val
+		T.types[keyName] = val
 		return false
-	},a...)
+	}, a...)
 	return T
 }
 
-func (T *SQLTable) pairArgs(a func(i int, k, v interface{}) bool, b ...interface{}){
-	var l = len(b)
+func (T *SQLTable) pairArgs(a func(i int, k, v interface{}) bool, b ...interface{}) {
+	l := len(b)
 	if l%2 != 0 {
 		return
 	}
-	for i:=0;i<l;i+=2 {
+	for i := 0; i < l; i += 2 {
 		if a(i, b[i], b[i+1]) {
 			return
 		}
 	}
 }
 
-//可以对字段名加引号或者其它什么的
-func (T *SQLTable) ColumnMark(q string) *SQLTable  {
-	T.columnMark=q
+// 可以对字段名加引号或者其它什么的
+func (T *SQLTable) ColumnMark(q string) *SQLTable {
+	T.columnMark = q
 	return T
 }
+
 func (T *SQLTable) addColumnMark(col string) string {
 	mark := T.columnMark
 	if mark == "" {
@@ -118,37 +118,40 @@ func (T *SQLTable) addColumnMark(col string) string {
 	}
 	for _, c := range col {
 		if c >= 'A' && c <= 'Z' {
-			return mark+ col +mark
+			return mark + col + mark
 		}
 	}
 	return col
 }
 
-//他会替换语句中的 $Values$ 符号
+// 他会替换语句中的 $Values$ 符号
 func (T *SQLTable) Value(column string, value interface{}) *SQLTable {
 	keyName := T.addColumnMark(column)
 	T.valuesKeys = append(T.valuesKeys, keyName)
 	T.valuesVals = append(T.valuesVals, value)
 	return T
 }
-//读取所有值
+
+// 读取所有值
 func (T *SQLTable) ValueValues(v ...interface{}) []interface{} {
-	vals := append(T.valuesVals, v ...)
+	vals := append(T.valuesVals, v...)
 	return quoteValue(vals)
 }
-//他会替换语句中的 $Values$ 符号
+
+// 他会替换语句中的 $Values$ 符号
 func (T *SQLTable) Values(a ...interface{}) *SQLTable {
 	return T.more(T.Value, a...)
 }
+
 func (T *SQLTable) values(s string) string {
 	var values []string
 	for index, val := range T.valuesVals {
 		keyName := T.valuesKeys[index]
 		keyVal := T.progress(val)
 		if typ, ok := T.types[keyName]; ok {
-			keyVal +="::"+typ
+			keyVal += "::" + typ
 		}
-		values	= append(values, keyVal)
+		values = append(values, keyVal)
 	}
 	if len(T.valuesKeys) > 0 {
 		valuesKeys := strings.Join(T.valuesKeys, ",")
@@ -160,53 +163,54 @@ func (T *SQLTable) values(s string) string {
 	return s
 }
 
-//他会替换语句中的 $Where$ 符号
+// 他会替换语句中的 $Where$ 符号
 func (T *SQLTable) And(column string, value interface{}) *SQLTable {
 	return T.Where("and", column, value)
 }
 
-//他会替换语句中的 $Where$ 符号
+// 他会替换语句中的 $Where$ 符号
 func (T *SQLTable) Ands(a ...interface{}) *SQLTable {
 	return T.more(T.And, a...)
 }
 
-//他会替换语句中的 $Where$ 符号
+// 他会替换语句中的 $Where$ 符号
 func (T *SQLTable) Or(column string, value interface{}) *SQLTable {
 	return T.Where("or", column, value)
 }
 
-//他会替换语句中的 $Where$ 符号
+// 他会替换语句中的 $Where$ 符号
 func (T *SQLTable) Ors(a ...interface{}) *SQLTable {
 	return T.more(T.Or, a...)
 }
-//他会替换语句中的 $Where$ 符号
+
+// 他会替换语句中的 $Where$ 符号
 func (T *SQLTable) Where(symbol, column string, value interface{}) *SQLTable {
 	T.swhere = append(T.swhere, &sqlTableWhere{symbol, column, value})
 	return T
 }
+
 func (T *SQLTable) where(s string) string {
 	var (
-	 	swhere []string
-	 	isOptional bool = strings.Contains(s, "*Where*")
-	 ) 
+		swhere     []string
+		isOptional bool = strings.Contains(s, "*Where*")
+	)
 	for _, w := range T.swhere {
-		if len(swhere)>0 || isOptional {
+		if len(swhere) > 0 || isOptional {
 			swhere = append(swhere, w.symbol)
 		}
 		swhere = append(swhere, strings.Replace(w.key, "?", T.progress(w.val), 1))
 	}
-	
+
 	where := strings.Join(swhere, " ")
 	s = strings.Replace(s, "*Where*", where, 1)
 	if len(swhere) != 0 {
-		where = "where "+where
+		where = "where " + where
 	}
 	s = strings.Replace(s, "$Where$", where, 1)
 	return s
-
 }
 
-//他会替换语句中的 $Set$ 符号
+// 他会替换语句中的 $Set$ 符号
 func (T *SQLTable) Set(column string, value interface{}) *SQLTable {
 	keyName := T.addColumnMark(column)
 	T.setKeys = append(T.setKeys, keyName)
@@ -214,17 +218,18 @@ func (T *SQLTable) Set(column string, value interface{}) *SQLTable {
 	return T
 }
 
-//他会替换语句中的 $Set$ 符号
+// 他会替换语句中的 $Set$ 符号
 func (T *SQLTable) Sets(a ...interface{}) *SQLTable {
 	return T.more(T.Set, a...)
 }
-func (T *SQLTable) more(f func(string, interface{}) *SQLTable ,a ...interface{}) *SQLTable {
-	var l = len(a)
+
+func (T *SQLTable) more(f func(string, interface{}) *SQLTable, a ...interface{}) *SQLTable {
+	l := len(a)
 	if l%2 != 0 {
 		return T
 	}
-	for i:=0;i<l;i+=2 {
-		c,ok := a[i].(string)
+	for i := 0; i < l; i += 2 {
+		c, ok := a[i].(string)
 		if !ok {
 			return T
 		}
@@ -233,62 +238,64 @@ func (T *SQLTable) more(f func(string, interface{}) *SQLTable ,a ...interface{})
 	}
 	return T
 }
+
 func (T *SQLTable) set(s string) string {
 	var (
-		sets []string
+		sets       []string
 		setsSerial []string
 	)
-	for index, keyName  := range T.setKeys {
+	for index, keyName := range T.setKeys {
 		val := T.setVals[index]
 		keyVal := T.progress(val)
 		if typ, ok := T.types[keyName]; ok {
-			keyVal +="::"+typ
+			keyVal += "::" + typ
 		}
 		sets = append(sets, fmt.Sprintf(`%s=%s`, keyName, keyVal))
 		setsSerial = append(setsSerial, keyVal)
 	}
-	
-	set := strings.Join(sets,",")
+
+	set := strings.Join(sets, ",")
 	s = strings.Replace(s, "*Set*", set, 1)
 	if len(sets) > 0 {
-		set = "set "+set
+		set = "set " + set
 	}
 	s = strings.Replace(s, "$Set$", set, 1)
-	s = strings.Replace(s, "$SetKeys$", strings.Join(T.setKeys,","), 1)
-	s = strings.Replace(s, "$SetSerial$", strings.Join(setsSerial,","), 1)
+	s = strings.Replace(s, "$SetKeys$", strings.Join(T.setKeys, ","), 1)
+	s = strings.Replace(s, "$SetSerial$", strings.Join(setsSerial, ","), 1)
 	return s
 }
 
-//读取所有值
+// 读取所有值
 func (T *SQLTable) SetValues(v ...interface{}) []interface{} {
-	vals := append(T.setVals, v ...)
+	vals := append(T.setVals, v...)
 	return quoteValue(vals)
 }
 
-//他会替换语句中的 $Excluded$ 符号
-//excluded 用于在插入存在，则更新时，排除的例名
+// 他会替换语句中的 $Excluded$ 符号
+// excluded 用于在插入存在，则更新时，排除的例名
 func (T *SQLTable) Excluded(a ...string) *SQLTable {
 	T.sexcluded = append(T.sexcluded, a...)
 	return T
 }
+
 func (T *SQLTable) excluded(s string) string {
 	var sets []string
-	for _, val  := range T.sexcluded {
+	for _, val := range T.sexcluded {
 		val = T.addColumnMark(val)
 		if sliceContainString(T.valuesKeys, val) {
 			sets = append(sets, fmt.Sprintf(`%[1]s=excluded.%[1]s`, val))
 		}
 	}
-	if len(sets) >0 {
-		s = strings.Replace(s,"$Excluded$", strings.Join(sets,","), 1)
+	if len(sets) > 0 {
+		s = strings.Replace(s, "$Excluded$", strings.Join(sets, ","), 1)
 	}
 	return s
 }
 
 type sqlTableWhere struct {
-	symbol	string
-	key		string
-	val		interface{}
+	symbol string
+	key    string
+	val    interface{}
 }
 
 func (T *SQLTable) assemble(source string) string {
@@ -299,16 +306,16 @@ func (T *SQLTable) assemble(source string) string {
 	return source
 }
 
-func (T *SQLTable) assembleStatement() string {
-	if T.result != ""{
-		return  T.result
+func (T *SQLTable) assembleStatement(args ...interface{}) string {
+	if T.result != "" {
+		return T.result
 	}
 	source := T.source
-	for index, arg := range T.extArgs {
+	for index, arg := range append(T.extArgs, args...) {
 		dollar := T.progress(arg)
 		essential := fmt.Sprint("@", index+1)
 		if strings.Contains(source, essential) {
-			source = strings.Replace(source, essential, dollar, -1)
+			source = strings.ReplaceAll(source, essential, dollar)
 			continue
 		}
 		source = strings.Replace(source, "?", dollar, 1)
@@ -317,25 +324,36 @@ func (T *SQLTable) assembleStatement() string {
 	return T.result
 }
 
-//返回sql语句，需要先调用.Args方法再调用该方法
+// 返回sql语句，需要先调用.Args方法再调用该方法
+//
 //	string 格式后的SQL句子
 func (T *SQLTable) SQL() string {
 	return T.assembleStatement()
 }
 
-//扩展参数
+func (T *SQLTable) clone() *SQLTable {
+	t := new(SQLTable)
+	*t = *T
+	t.result = ""
+	t.count = 0
+	t.args = t.args[:0]
+	return t
+}
+
+// 扩展参数
+//
 //	args ...interface{}		额外参数，在非解析SQL句子里的？
 func (T *SQLTable) ExtArgs(args ...interface{}) *SQLTable {
-	T.extArgs = append(T.extArgs, args...)
-	return T
+	t := T.clone()
+	t.extArgs = append(T.extArgs, args...)
+	return t
 }
 
-//载入参数，他会替换语句中的？符号
+// 载入参数，他会替换语句中的？符号
+//
 //	args ...interface{}		额外参数，在非解析SQL句子里的？
 //	[]interface{}			返回所有参数
-func (T *SQLTable) Args(args ...interface{}) []interface{}{
-	T.extArgs = append(T.extArgs, args...)
-	T.assembleStatement()
+func (T *SQLTable) Args(args ...interface{}) []interface{} {
+	T.assembleStatement(args...)
 	return T.args
 }
-
--- sqltable/sql_test.go
+++ sqltable/sql_test.go
@@ -2,25 +2,57 @@ package sqltable
 
 import (
 	"testing"
+
+	"github.com/issue9/assert/v2"
 )
 
-func Test_NewSQLTable_Where(t *testing.T){
-	var ssql1 = Prepare(`select * from "B" $Where$`).Ors(`"G1"=?`,1,`"G2"=?`,2)
-	var ssql = Prepare(`select * from "A" $Where$`).And(`"B1"=?`,1).And(`"B3"=?`,3).Where("and","B4=?",ssql1)
-	t.Log(ssql.SQL())
+func Test_NewSQLTable_Where(t *testing.T) {
+	as := assert.New(t, true)
+	ssql1 := Prepare(`select * from "B" $Where$`).Ors(`"G1"=?`, 1, `"G2"=?`, 2)
+	ssql := Prepare(`select * from "A" $Where$`).And(`"B1"=?`, 1).And(`"B3"=?`, 3).Where("and", "B4=?", ssql1)
+
+	as.Equal(ssql.Args(), []interface{}{1, 3, 1, 2})
+	as.Equal(ssql.SQL(), "select * from \"A\" where \"B1\"=$1 and \"B3\"=$2 and B4=(select * from \"B\" where \"G1\"=$3 or \"G2\"=$4)")
 }
 
-func Test_NewSQLTable_Values(t *testing.T){
-	var ssql1 = Prepare(`select * from "B" $Where$`).Or(`"G1"=?`,1).Or(`"G2"=?`,2)
-	var ssql = Prepare(`insert into "B" $Values$ $Where$`).Values(`A1`,1,`A2`,1).Ands(`"B3"=?`,3,`"B1"=?`,1).Where("and","B4=?",ssql1)
-	t.Log(ssql.SQL())
+func Test_NewSQLTable_Values(t *testing.T) {
+	as := assert.New(t, true)
+
+	ssql1 := Prepare(`select * from "B" $Where$`).Or(`"G1"=?`, 1).Or(`"G2"=?`, 2)
+	ssql := Prepare(`insert into "B" $Values$ $Where$`).Values(`A1`, 1, `A2`, 1).Ands(`"B3"=?`, 3, `"B1"=?`, 1).Where("and", "B4=?", ssql1)
+	as.Equal(ssql.Args(), []interface{}{3, 1, 1, 2, 1, 1})
+	as.Equal(ssql.SQL(), "insert into \"B\" (\"A1\",\"A2\") values($5,$6) where \"B3\"=$1 and \"B1\"=$2 and B4=(select * from \"B\" where \"G1\"=$3 or \"G2\"=$4)")
 }
 
-func Test_NewSQLTable_Set(t *testing.T){
-	var ssql1 = Prepare(`select * from "B" $Where$ limit ?`).Or(`"G1"=?`,1).Or(`"G2"=?`,2)
-	ssql1.ExtArgs(1)
-	var ssql = Prepare(`update into "A" $Set$ $Where$ limit ?`).Sets("A1",1,"A2",2).And(`"B1"=?`,1).And(`"B3"=?`,3).Where("and","B4=?",ssql1)
-	t.Log(ssql.Args(2), ssql.SQL())
+func Test_NewSQLTable_Set(t *testing.T) {
+	as := assert.New(t, true)
+	ssql1 := Prepare(`select * from "B" $Where$ limit ?`).Or(`"G1"=?`, 6).Or(`"G2"=?`, 7)
+	ssql1 = ssql1.ExtArgs(4)
+	ssql := Prepare(`update into "A" $Set$ $Where$ limit ?`).Sets("A1", 2, "A2", 3).And(`"B1"=?`, 4).And(`"B3"=?`, 5).Where("and", "B4=?", ssql1)
+	as.Equal(ssql.Args(1), []interface{}{1, 4, 5, 4, 6, 7, 2, 3})
+	as.Equal(ssql.SQL(), "update into \"A\" set \"A1\"=$7,\"A2\"=$8 where \"B1\"=$2 and \"B3\"=$3 and B4=(select * from \"B\" where \"G1\"=$5 or \"G2\"=$6 limit $4) limit $1")
 }
 
+func Test_NewSQLTable_progress(t *testing.T) {
+	as := assert.New(t, true)
+	t1 := Prepare(`select id from t1 where a=? and b=?`)
+	t1 = t1.ExtArgs(1)
+	t1_1 := t1.ExtArgs(2)
+	t1_2 := t1.ExtArgs(3)
 
+	t2 := Prepare(`inster into t2 $Values$`)
+	t2.Values("a", t1_1, "b", t1_2)
+
+	as.Equal(t2.Args(), []interface{}{1, 2, 1, 3}).Equal(t2.SQL(), "inster into t2 (a,b) values((select id from t1 where a=$1 and b=$2),(select id from t1 where a=$3 and b=$4))")
+
+	as.Equal(t1.Args(4), []interface{}{1, 4}).Equal(t1.SQL(), "select id from t1 where a=$1 and b=$2")
+
+	as.Equal(t1_1.Args(), []interface{}{1, 2}).Equal(t1_1.SQL(), "select id from t1 where a=$1 and b=$2")
+
+	as.Equal(t1_2.Args(), []interface{}{1, 3}).Equal(t1_2.SQL(), "select id from t1 where a=$1 and b=$2")
+
+	t3 := Prepare(`select * from t3 $Where$`)
+	t3.Ands("a=?", t2, "b=?", 5)
+
+	as.Equal(t3.Args(), []interface{}{1, 2, 1, 3, 5}).Equal(t3.SQL(), "select * from t3 where a=(inster into t2 (a,b) values((select id from t1 where a=$1 and b=$2),(select id from t1 where a=$3 and b=$4))) and b=$5")
+}
